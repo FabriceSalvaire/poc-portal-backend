cfcd5443d11793c88a4ac804d59b3e92b5c4a57d
Fabrice SALVAIRE <fabrice.salvaire@orange.fr>
2020-11-09 00:32:30 +0100

    add BACKEND_SETTINGS_PATH

--- app/setenv.sh
+++ app/setenv.sh
@@ -2,6 +2,8 @@ source env/bin/activate
 append_to_python_path_if_not ${PWD}
 source <(invoke --print-completion-script bash)
 
+export BACKEND_SETTINGS_PATH='dev.env'
+
 ####################################################################################################
 #
 # Environments
